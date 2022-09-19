884327d5b5b7b563b2ed676eaafc8e2893b37c52
leejinsil0408 <hby04011@naver.com>
2022-09-19 15:48:56 +0900

    20220919 study

--- src/main/java/com/example/demo/Domain/Board.java
+++ src/main/java/com/example/demo/Domain/Board.java
@@ -14,26 +14,11 @@ import java.util.Date;
 @Entity
 
 public class Board {
-    @Id @GeneratedValue
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long seq;
-
-    @Column(length = 40, nullable = false)
     private String title;
-
-    @Column(updatable = false, nullable = false)
     private String writer;
-
-    @Column(nullable = false)
-    @ColumnDefault("'no content'")
     private String content;
-
-    //시간 제약
-    @Temporal(TemporalType.DATE)
-    //데이터 값 삽입 불가, 값 변경 불가
-    @Column(insertable = false, updatable = false, columnDefinition = "date dafault sysdate" )
     private Date createDate;
-
-    @ColumnDefault("0")
-    @Column(insertable = false, updatable = false, columnDefinition = "number default 0")
     private Long cnt;
 }
